4869b678cdc6de864225f62c8d20e03964ab6a65
Matt Edwards <mattsp.edwards@googlemail.com>
2023-05-13 14:17:42 +0100

    feat: added podcast and episode serializer

--- podcasts/serializers.py
+++ podcasts/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Podcast
+from .models import Podcast, Episode
 
 class PodcastSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +10,20 @@ class PodcastSerializer(serializers.ModelSerializer):
             'name',
             'channel_link',
         ]
+
+class EpisodeSerializer(serializers.ModelSerializer):
+    channel = serializers.SerializerMethodField()
+    class Meta:
+        model = Episode
+        fields = [
+            'video_id',
+            'channel',
+            'title',
+            'transcript',
+            'times_clicked',
+        ]
+
+    def get_channel(self, obj):
+        channel = Podcast.objects.get(id=obj.channel.id)
+        serializer = PodcastSerializer(channel, many=False)
+        return serializer.data
