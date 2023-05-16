592d44a3ebadd4731ee8df28a5de0493964cdaaa
Matt Edwards <mattsp.edwards@googlemail.com>
2023-05-16 22:01:34 +0100

    refactor: added no of episodes to podcastpanel

--- frontend/src/components/PodcastPanel.js
+++ frontend/src/components/PodcastPanel.js
@@ -3,6 +3,7 @@ import {
     CardActionArea,
     CardContent,
     Typography,
+    Grid,
 } from '@mui/material'
 
 export default function PodcastPanel({ podcast }) {
@@ -10,9 +11,16 @@ export default function PodcastPanel({ podcast }) {
         <Card>
             <CardActionArea href={podcast.channel_link} target='_blank'>
                 <CardContent>
-                    <Typography variant='h6' component='span' fontWeight={700}>
-                        {podcast.name}
-                    </Typography>
+                    <Grid container spacing={2}>
+                        <Grid item xs={8}>
+                            <Typography variant='h6' component='span' fontWeight={700}>
+                                {podcast.name} 
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={4}>
+                            No. {podcast.no_of_episodes}
+                        </Grid>
+                    </Grid>
                 </CardContent>
             </CardActionArea>
         </Card>
