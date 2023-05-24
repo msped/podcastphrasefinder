551d4480018fe65fa6ce176db4b22ba519d4f930
Matt Edwards <mattsp.edwards@googlemail.com>
2023-05-24 21:54:00 +0100

    feat: added useGetSearchEpisodesHook

---
+++ frontend/src/hooks/useGetEpisodesSearchHook.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+import getEpisodesSearchService from '@/api/getEpisodesSearchService'
+
+const useGetEpisodesSearchHook = (query) => {
+    const [results, setResults] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        const fetchDataFromService = () => {
+            getEpisodesSearchService(query)
+            .then(setResults)
+            setIsLoading(false)
+        }
+
+        if (query.length >= 3 && !results.length > 0) {
+            fetchDataFromService();
+        }
+        else if (query.length === 0 && isLoading && results.length === 0) {
+            fetchDataFromService();
+        } else {
+            const timeoutId = setTimeout(() => {
+                if (query) {
+                    fetchDataFromService();
+                }
+            }, 750)
+            return () => {
+                clearTimeout(timeoutId)
+            }
+        }
+    }, [query])
+
+    return { results, isLoading };
+}
+
+export default useGetEpisodesSearchHook;
