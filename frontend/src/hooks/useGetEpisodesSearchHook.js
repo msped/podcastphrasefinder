6da36fe60ca37680c525583fbd5dac684fff3588
Matt Edwards <mattsp.edwards@googlemail.com>
2023-05-27 16:15:15 +0100

    refactor: remove first api call

--- frontend/src/hooks/useGetEpisodesSearchHook.js
+++ frontend/src/hooks/useGetEpisodesSearchHook.js
@@ -14,9 +14,6 @@ const useGetEpisodesSearchHook = (query) => {
 
         if (query.length >= 3 && !results.length > 0) {
             fetchDataFromService();
-        }
-        else if (query.length === 0 && isLoading && results.length === 0) {
-            fetchDataFromService();
         } else {
             const timeoutId = setTimeout(() => {
                 if (query) {
