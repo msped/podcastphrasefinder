d714461f279a64f7f2c883ea3dfb06dfc596f6e1
Matt Edwards <mattsp.edwards@googlemail.com>
2023-05-27 16:40:58 +0100

    test: added test for useGetEpisodesSearchService hook

---
+++ frontend/src/__tests__/hooks/useGetEpisodesSearchService.test.js
@@ -0,0 +1,104 @@
+import { renderHook, waitFor, act } from '@testing-library/react';
+import getEpisodesSearchService from '@/api/getEpisodesSearchService';
+import useGetEpisodesSearchHook from '@/hooks/useGetEpisodesSearchHook';
+
+jest.mock('../../api/getEpisodesSearchService');
+
+describe('useGetEpisodesSearchHook', () => {
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
+
+    it('fetches data when query length is >=3 and results are empty', () => {
+        const query = 'test';
+        const mockData = {
+            isLoading: true,
+            results: [{
+                "id": 1,
+                "video_id": "b-UYSj6Q0Ao",
+                "channel": {
+                    "id": 2,
+                    "name": "The Rest Is Politics",
+                    "channel_link": "https://www.youtube.com/@restispolitics",
+                    "no_of_episodes": 1
+                },
+                "title": "Suella's speeding, Japan in focus, and what's the point of the G7?",
+                "transcript": "This is a test transcript of a podcast from the Rest is Politics.",
+                "times_clicked": 11
+            }]
+        }
+
+        getEpisodesSearchService.mockResolvedValueOnce(mockData);
+
+        const { result, rerender } = renderHook(() =>
+            useGetEpisodesSearchHook(query)
+        );
+
+        waitFor(() => expect(result.current.results).toEqual([]));
+        waitFor(() => expect(result.current.isLoading).toBe(true));
+
+        rerender();
+
+        waitFor(() => expect(result.current.results).toEqual(mockData));
+        waitFor(() =>expect(result.current.isLoading).toBe(false));
+    });
+
+    it('fetches data when query length is 0 and isLoading is true and results are empty', () => {
+        const query = '';
+        const mockData = {
+            isLoading: true,
+            results: []
+        }
+
+        getEpisodesSearchService.mockResolvedValueOnce(mockData);
+
+        const { result, rerender } = renderHook(() =>
+            useGetEpisodesSearchHook(query)
+        );
+
+        waitFor(() => expect(result.current.results).toEqual([]));
+        waitFor(() => expect(result.current.isLoading).toBe(true));
+
+        rerender();
+
+        waitFor(() => expect(result.current.results).toEqual(mockData));
+        waitFor(() => expect(result.current.isLoading).toBe(false));
+    });
+
+    it('debounces the API call for queries longer than 2 characters', async () => {
+        let query = 'test';
+        const mockData = {
+            isLoading: true,
+            results: []
+        }
+
+        getEpisodesSearchService.mockResolvedValueOnce(mockData);
+
+        const { result, rerender } = renderHook(() =>
+            useGetEpisodesSearchHook(query)
+        );
+
+        waitFor(() => expect(result.current.results).toEqual([]));
+        waitFor(() => expect(result.current.isLoading).toBe(true));
+
+        // Wait for the first API call to finish
+        rerender();
+
+        getEpisodesSearchService.mockResolvedValueOnce([]);
+
+        // Change the query and trigger a new API call
+        act(() => {
+            query = 'new query'
+        });
+
+        // Expect the results to still be the same, since the new API call has been debounced
+        waitFor(() => expect(result.current.results).toEqual(mockData));
+        waitFor(() => expect(result.current.isLoading).toBe(true));
+
+        // Wait for the second API call to finish
+        rerender();
+
+        waitFor(() => expect(result.current.results).toEqual([]));
+        waitFor(() => expect(result.current.isLoading).toBe(false));
+    });
+});
