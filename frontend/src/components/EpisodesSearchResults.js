e716427d3445f9a78458f93dceeb0c4a864d593a
Matt Edwards <mattsp.edwards@googlemail.com>
2023-05-28 18:41:14 +0100

    feat: added EpisodesSearchResults component

---
+++ frontend/src/components/EpisodesSearchResults.js
@@ -0,0 +1,47 @@
+import useGetEpisodesSearchHook from '@/hooks/useGetEpisodesSearchHook'
+import EpisodePanel from '@/components/EpisodePanel'
+import { Grid, Stack, Typography } from '@mui/material'
+
+import LoadingSpinner from './LoadingSpinner'
+
+export default function EpisodesSearchResults({ query }) {
+    const { results, isLoading } = useGetEpisodesSearchHook(query)
+
+    if (isLoading && query.length > 3) {
+        return (
+            <Stack alignItems='center' alignContent='center' my={5}>
+                <LoadingSpinner />
+            </Stack>
+        )
+    }
+
+    if (isLoading && results.length === 0) {
+        return (
+            <Stack alignItems='center' alignContent='center'>
+                <Typography variant='body1' component='p' sx={{textAlign: 'center'}}>
+                    Type the phrase you heard above to find the episode you heard it from.
+                </Typography>
+            </Stack>
+        )
+    }
+
+    if (!isLoading && results.length === 0) {
+        return (
+            <Stack alignItems='center' alignContent='center'>
+                <Typography>
+                    No results
+                </Typography>
+            </Stack>
+        )
+    }
+
+    return (
+        <Grid container spacing={2}>
+            {results.map((item) => (
+                <Grid item key={item.id} xs={12}>
+                    <EpisodePanel episode={item}/>
+                </Grid>
+            ))}
+        </Grid>
+    )
+}
