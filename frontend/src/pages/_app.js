a17730f2729f2f7ce4b61555d260dde2625ce048
Matt Edwards <mattsp.edwards@googlemail.com>
2023-06-04 14:40:39 +0100

    feat: add Footer to _app.js

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -5,6 +5,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider } from '@emotion/react';
 import theme from '@/theme';
 import createEmotionCache from '@/createEmotionCache';
+import Footer from '@/components/Footer';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -21,6 +22,7 @@ export default function MyApp(props) {
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Component {...pageProps} />
+        <Footer />
       </ThemeProvider>
     </CacheProvider>
   );
