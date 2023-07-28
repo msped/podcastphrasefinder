19761bb0fa7931c634764aaa5f513888b2cac753
Matt Edwards <mattsp.edwards@googlemail.com>
2023-07-28 23:07:26 +0100

    tests: added tests for the home pagez

---
+++ frontend/src/__tests__/pages/Home.test.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import "@testing-library/jest-dom"
+import { screen, render, waitFor, fireEvent } from "@testing-library/react"
+import mockRouter from 'next-router-mock';
+import Home from '@/pages/index';
+import { toBeInTheDocument } from '@testing-library/jest-dom/dist/matchers';
+
+jest.mock('next/router', () => require('next-router-mock'))
+
+jest.mock('../../hooks/useGetMostPopularHook', () => ({
+    __esModule: true,
+    default: jest.fn(() => ({
+        episode: {
+            id: 1,
+            title: 'Episode 1',
+            video_id: 'AZLU_4G8CY0',
+            channel: {
+                name: 'Dead Men Talking',
+                channel_link: 'https://www.youtube.com/@DeadMenTalkingPod'
+            },
+        },
+        isLoading: false,
+    })),
+}));
+
+describe('Home component', () => {
+    it('renders "PodFinder" header text', () => {
+        render(<Home />);
+        expect(screen.queryAllByText(/PodFinder/i)[0]).toBeInTheDocument();
+    });
+
+    it('renders the episodes page from the home page', () => {
+        mockRouter.push('/')
+
+        const { getByRole } = render(<Home />)
+        expect(mockRouter).toMatchObject({
+            pathname: '/'
+        })
+
+        fireEvent.click(getByRole('link', { name: /Go to Episodes/i }))
+
+        expect(screen.getByText('Search for an Episode')).toBeInTheDocument();
+        waitFor(() => {
+            expect(mockRouter).toMatchObject({
+                pathname: '/episodes'
+            })
+        })
+    })
+
+    it('renders the podcasts page from the home page', () => {
+        mockRouter.push('/')
+
+        const { getByRole } = render(<Home />)
+        expect(mockRouter).toMatchObject({
+            pathname: '/'
+        })
+
+        fireEvent.click(getByRole('link', { name: /Go to Podcasts/i }))
+
+        expect(screen.getByText('Search Podcasts')).toBeInTheDocument();
+        waitFor(() => {
+            expect(mockRouter).toMatchObject({
+                pathname: '/podcasts'
+            })
+        })
+    })
+
+    it('renders the podcasts youtube page in the most popular card', () => {
+        render(<Home />)
+        expect(
+            screen.getByRole('link', { name: /Dead Men Talking/i })
+        ).toBeInTheDocument();
+    })
+    
+});
