cd009056ca71960f83419417dc69ed1c2dbde935
Matt Edwards <mattsp.edwards@googlemail.com>
2023-07-29 17:34:02 +0100

    feat: added nextjs head

--- frontend/src/pages/podcasts/index.js
+++ frontend/src/pages/podcasts/index.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Head from 'next/head';
 import { 
     Container,
     Grid,
@@ -16,44 +17,51 @@ export default function Podcasts() {
     const [query, setQuery] = useState('')
 
     return (
-        <Container maxWidth='sm' sx={{ marginY: 2 }}>
-            <Grid container spacing={2}>
-                <Grid item xs={12}>
-                    <Stack 
-                        direction='column' 
-                        alignItems='center' 
-                        alignContent='center'
-                    >
-                        <Typography variant='h2' component='h1' fontWeight={500}>
-                            Podcasts
-                        </Typography>
-                        <Typography variant='subtitle1' fontWeight={500}>
-                            See all the Podcasts and the amount of episodes we have transcripts for.
-                        </Typography>
-                    </Stack>
+        <>
+            <Head>
+                <title>Find which podcasts and how many episodes are currently supported. | PodFinder</title>
+                <meta name="description" content="Find which podcasts and how many episodes are currently supported." />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Container maxWidth='sm' sx={{ marginY: 2 }}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <Stack 
+                            direction='column' 
+                            alignItems='center' 
+                            alignContent='center'
+                        >
+                            <Typography variant='h2' component='h1' fontWeight={500}>
+                                Podcasts
+                            </Typography>
+                            <Typography variant='subtitle1' fontWeight={500}>
+                                See all the Podcasts and the amount of episodes we have transcripts for.
+                            </Typography>
+                        </Stack>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Input
+                            id="outlined-search"
+                            type="search"
+                            variant='filled'
+                            onChange={(e) => setQuery(e.target.value)}
+                            fullWidth
+                            value={query}
+                            endAdornment={
+                                <InputAdornment position="end" >
+                                    <IconButton edge="end">
+                                        <SearchIcon />
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <PodcastsSearchResults query={query} />
+                    </Grid>
                 </Grid>
-                <Grid item xs={12}>
-                    <Input
-                        id="outlined-search"
-                        type="search"
-                        variant='filled'
-                        onChange={(e) => setQuery(e.target.value)}
-                        fullWidth
-                        value={query}
-                        endAdornment={
-                            <InputAdornment position="end" >
-                                <IconButton edge="end">
-                                    <SearchIcon />
-                                </IconButton>
-                            </InputAdornment>
-                        }
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <PodcastsSearchResults query={query} />
-                </Grid>
-            </Grid>
-        </Container>
+            </Container>
+        </>
     )
-
 }
