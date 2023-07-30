927f08b7077208bfdfae73dca12e11a02409d42c
Matt Edwards <mattsp.edwards@googlemail.com>
2023-07-30 23:34:17 +0100

    feat: imported signals into PodcastsConfig

--- podcasts/apps.py
+++ podcasts/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class PodcastsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'podcasts'
+
+    def ready(self):
+        import podcasts.signals
