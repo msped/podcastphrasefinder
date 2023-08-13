8a453ce390b71aabcd9a0b5f319c167d93750502
Matt Edwards <mattsp.edwards@googlemail.com>
2023-08-13 16:35:34 +0100

    refactor: added if statement to stop tasks in test

--- podcasts/signals.py
+++ podcasts/signals.py
@@ -1,3 +1,4 @@
+from django.db import connection
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -6,4 +7,5 @@ from .tasks import add_back_catalogue_task
 
 @receiver(post_save, sender=Podcast)
 def add_back_catalogue_of_channel(sender, instance, **kwargs):
-    add_back_catalogue_task.delay(instance.id, instance.channel_id, instance.video_filter)
+    if connection.settings_dict['NAME'] != 'testdatabase':
+        add_back_catalogue_task.delay(instance.id, instance.channel_id, instance.video_filter)
