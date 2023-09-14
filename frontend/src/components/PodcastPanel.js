1f0e734283f923fc1751539bbd8e0cd401aa7e18
Matt Edwards <mattsp.edwards@googlemail.com>
2023-09-15 00:58:08 +0100

    feat: added avatar to podcastpanel

--- frontend/src/components/PodcastPanel.js
+++ frontend/src/components/PodcastPanel.js
@@ -4,6 +4,8 @@ import {
     CardContent,
     Typography,
     Grid,
+    Avatar,
+    Stack,
 } from '@mui/material'
 
 export default function PodcastPanel({ podcast }) {
@@ -14,9 +16,19 @@ export default function PodcastPanel({ podcast }) {
                 <CardContent>
                     <Grid container spacing={2}>
                         <Grid item xs={8}>
-                            <Typography variant='h6' component='span' fontWeight={700}>
-                                {podcast.name} 
-                            </Typography>
+                            <Stack direction='row' spacing={2}>
+                                <Avatar
+                                    src={podcast.avatar}
+                                    alt={podcast.name}
+                                    sx={{
+                                        height: 40,
+                                        width: 40
+                                    }}
+                                />
+                                <Typography variant='h6' component='span' fontWeight={700}>
+                                    {podcast.name} 
+                                </Typography>
+                            </Stack>
                         </Grid>
                         <Grid item xs={4}>
                             No. {podcast.no_of_episodes}
