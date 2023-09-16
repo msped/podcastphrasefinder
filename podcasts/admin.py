061d6cd6774ee1521eb14545bc57359abced019c
Matt Edwards <mattsp.edwards@googlemail.com>
2023-09-17 00:20:44 +0100

    feat: added EpisodeReleaseDay model to Podcast admin

--- podcasts/admin.py
+++ podcasts/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 
-from .models import Episode, Podcast
+from .models import Episode, Podcast, EpisodeReleaseDay
 
 admin.site.register(Episode)
-admin.site.register(Podcast)
+admin.site.register(EpisodeReleaseDay)
+
+class EpisodeReleaseDayInlineAdmin(admin.TabularInline):
+    model = EpisodeReleaseDay
+    extra = 1
+
+@admin.register(Podcast)
+class PodcastAdmin(admin.ModelAdmin):
+    inlines = [EpisodeReleaseDayInlineAdmin]
+
+    class Meta:
+        model = Podcast
