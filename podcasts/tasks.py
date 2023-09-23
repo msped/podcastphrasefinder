bf2638b361c9dce9a438f126583d527f74611d12
Matt Edwards <mattsp.edwards@googlemail.com>
2023-09-24 00:01:19 +0100

    feat: added get_new_episodes function

--- podcasts/tasks.py
+++ podcasts/tasks.py
@@ -1,12 +1,13 @@
 from __future__ import absolute_import, unicode_literals
 import os
+from datetime import datetime as date
 from html import unescape
 from urllib.parse import urlencode
 from celery import shared_task
 from celery.utils.log import get_task_logger
 from django.db import transaction
 
-from .models import Episode, Podcast
+from .models import Episode, Podcast, EpisodeReleaseDay
 from .utils import call_api, get_transcript, check_for_private_video
 
 api_key = os.environ.get('YOUTUBE_V3_API_KEY')
@@ -26,7 +27,6 @@ def add_back_catalogue_task(channel_id, yt_channel_id, video_filter):
     }
 
     while True:
-        next_page_token = ""
         api_url = (
             'https://www.googleapis.com/youtube/v3/search?'
             + urlencode(url_params)
@@ -97,4 +97,65 @@ def check_avatar():
         if response_avatar != podcast.avatar:
             podcast.avatar = response_avatar
             podcast.save()
-    
+
+@shared_task
+def get_new_episodes():
+    # Gets day of the week sunday 1, monday 2 etc
+    today = (date.today().isoweekday() % 7) + 1
+    video_data = []
+
+    for podcast in EpisodeReleaseDay.objects.filter(day=today):
+        podcast_episode_qs = Episode.objects.filter(
+            channel__id=podcast.podcast.id
+        )
+
+        url_params = {
+            'key': api_key,
+            'channelId': podcast.podcast.channel_id,
+            'part': 'snippet,id',
+            'order': 'date',
+            'maxResults': 50,
+        }
+
+        while True:
+            video_break = False
+            api_url = (
+                'https://www.googleapis.com/youtube/v3/search?'
+                + urlencode(url_params)
+            )
+            response = call_api(api_url)
+
+            next_page_token = response.get("nextPageToken", "")
+
+            if next_page_token != "":
+                url_params['pageToken'] = next_page_token
+
+            videos = response.get('items', [])
+
+            for video in videos:
+                video_id = video['id']['videoId']
+                if video['id']['kind'] == 'youtube#video' and not \
+                        podcast_episode_qs.filter(video_id=video_id).exists():
+                    video_title = unescape(video['snippet']['title'])
+                    if not podcast.podcast.video_filter or \
+                            podcast.podcast.video_filter in video_title:
+                        transcript, error = get_transcript(video_id)
+                        video_data.append({
+                            'video_id': video_id,
+                            'channel_id': podcast.podcast.id,
+                            'title': video_title,
+                            'transcript': transcript,
+                            'error_occurred': error,
+                            'thumbnail': 'https://img.youtube.com/vi/' + \
+                                        f'{video_id}/maxresdefault.jpg',
+                            'published_date': video['snippet']['publishedAt']
+                        })
+                        logger.info(f"{video_title} - Error: {error}")
+                else:
+                    video_break = True
+
+            if next_page_token == "" or video_break:
+                break
+
+    if len(video_data) > 0:
+        Episode.objects.bulk_create([Episode(**data) for data in video_data])
