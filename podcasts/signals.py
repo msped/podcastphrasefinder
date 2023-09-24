17f8e5f49698bfa05358f097898002d6cfcd2b52
Matt Edwards <mattsp.edwards@googlemail.com>
2023-09-24 14:52:37 +0100

    refactor: removed code for get_avatar function

--- podcasts/signals.py
+++ podcasts/signals.py
@@ -1,31 +1,16 @@
-import os
-from urllib.parse import urlencode
 from django.db import connection
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from .models import Podcast
 from .tasks import add_back_catalogue_task
-from .utils import call_api
-
-api_key = os.environ.get('YOUTUBE_V3_API_KEY')
+from .utils import get_avatar
 
 @receiver(post_save, sender=Podcast)
 def add_back_catalogue_of_channel(sender, instance, **kwargs):
     if connection.settings_dict['NAME'] != 'testdatabase':
         if not instance.avatar:
-            url_params = {
-                'key': api_key,
-                'id': instance.channel_id,
-                'part': 'snippet',
-            }
-            api_url = (
-                'https://www.googleapis.com/youtube/v3/channels?'
-                + urlencode(url_params)
-            )
-            response = call_api(api_url)
-            channel = response.get('items', [])
-            instance.avatar = channel[0]['snippet']['thumbnails']['high']['url']
+            instance.avatar = get_avatar(instance.channel_id)
         if instance.run_auto_add_back_catalogue and not instance.has_add_back_catalogue_ran:
             add_back_catalogue_task.delay(instance.id, instance.channel_id, instance.video_filter)
             instance.has_add_back_catalogue_ran = True
