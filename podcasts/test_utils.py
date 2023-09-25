0e17a75f7a9c3b4e66282223cc3945a9ef47652f
Matt Edwards <mattsp.edwards@googlemail.com>
2023-09-25 23:29:32 +0100

    refactor: added mocks for externals apis in utils testing

--- podcasts/test_utils.py
+++ podcasts/test_utils.py
@@ -35,20 +35,29 @@ class TestUtils(TestCase):
         self.assertIsNone(result)
         mock_get.assert_called_once_with('https://example.com/api', timeout=10)
 
-    def test_get_transcript_failure(self):
+    @patch('youtube_transcript_api.YouTubeTranscriptApi.get_transcript')
+    def test_get_transcript_failure(self, mock_get_transcript):
+        expected_error_message = 'Could not retrieve a transcript for ' \
+            'the video https://www.youtube.com/watch?v=aVsz7OP-AcQ'
+        mock_get_transcript.side_effect = Exception(expected_error_message)
         transcript, error = get_transcript('aVsz7OP-AcQ')
         self.assertIn(
-            'Could not retrieve a transcript for ' + 
-            'the video https://www.youtube.com/watch?v=aVsz7OP-AcQ',
+            expected_error_message,
             transcript
         )
         self.assertTrue(error)
 
-    def test_check_for_private_video_true(self):
+    @patch('podcasts.utils.call_api')
+    def test_check_for_private_video_true(self, mock_call_api):
+        mock_call_api.return_value = {}
+        mock_call_api.status_code = 404
         response = check_for_private_video('Xw1EKgEl_RY')
         self.assertTrue(response)
 
-    def test_check_for_private_video_false(self):
+    @patch('podcasts.utils.call_api')
+    def test_check_for_private_video_false(self, mock_call_api):
+        mock_call_api.return_value = {}
+        mock_call_api.status_code = 200
         response = check_for_private_video('7moEbc-xYF8')
         self.assertFalse(response)
 
