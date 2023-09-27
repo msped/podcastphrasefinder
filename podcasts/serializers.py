6ff2b683243764094f1927ff6d20b69447190ccb
Matt Edwards <mattsp.edwards@googlemail.com>
2023-09-27 22:12:31 +0100

    refactor: serializer to work with EpisodeDocument

--- podcasts/serializers.py
+++ podcasts/serializers.py
@@ -3,24 +3,17 @@ from rest_framework import serializers
 from .models import Podcast, Episode
 
 class PodcastSerializer(serializers.ModelSerializer):
-    no_of_episodes = serializers.SerializerMethodField()
     class Meta:
         model = Podcast
         fields = [
             'id',
             'name',
             'channel_id',
-            'no_of_episodes',
             'avatar',
         ]
 
-    def get_no_of_episodes(self, obj):
-        return Episode.objects.filter(
-            channel_id=obj.id, error_occurred=False, private_video=False
-        ).count()
-
 class EpisodeSerializer(serializers.ModelSerializer):
-    channel = serializers.SerializerMethodField()
+    channel = PodcastSerializer(many=False, read_only=True)
     class Meta:
         model = Episode
         fields = [
@@ -32,9 +25,6 @@ class EpisodeSerializer(serializers.ModelSerializer):
             'thumbnail',
             'times_clicked',
             'published_date',
+            'error_occurred',
+            'private_video'
         ]
-
-    def get_channel(self, obj):
-        channel = Podcast.objects.get(id=obj.channel.id)
-        serializer = PodcastSerializer(channel, many=False)
-        return serializer.data
