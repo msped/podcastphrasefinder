daf32df032693e72e8d155e7786362f75724a340
Matt Edwards <mattsp.edwards@googlemail.com>
2023-09-27 22:00:59 +0100

    feat: added EpisodeDocument for ElasticSearch

---
+++ podcasts/documents.py
@@ -0,0 +1,32 @@
+from django_elasticsearch_dsl import Document, fields
+from django_elasticsearch_dsl.registries import registry
+
+from .models import Episode
+
+@registry.register_document
+class EpisodeDocument(Document):
+
+    channel = fields.ObjectField(
+        properties = {
+            "name": fields.TextField(),
+            "channel_id": fields.TextField(),
+            "avatar": fields.TextField()
+        }
+    )
+
+    class Index:
+        name = "episodes"
+
+    class Django:
+        model = Episode
+        fields = [
+            'id',
+            'video_id',
+            'title',
+            'transcript',
+            'times_clicked',
+            'thumbnail',
+            'published_date',
+            'private_video',
+            'error_occurred'
+        ]
