f3fec81695b377fc1a1daa92d11a6c878e834f57
Matt Edwards <mattsp.edwards@googlemail.com>
2023-10-08 20:07:59 +0100

    refactor: avatar to be centered in mobile view

--- frontend/src/components/PodcastInformation.js
+++ frontend/src/components/PodcastInformation.js
@@ -24,6 +24,20 @@ export default function PodcastInformation({ channelId }) {
                 <CardActionArea href={youtubeChannelLink} target='_blank'>
                     <CardContent sx={{ padding: 3 }}>
                         <Stack direction='column' spacing={1}>
+                            <Stack alignItems='center' justifyItems='center'>
+                                <Avatar
+                                    alt={podcast.name}
+                                    src={podcast.avatar}
+                                    sx={{
+                                        height: 60,
+                                        width: 60,
+                                        display: {
+                                            xs: 'inline-block',
+                                            sm: 'none'
+                                        }
+                                    }}
+                                />
+                            </Stack>
                             <Stack direction='row' spacing={2} marginY={1}>
                                 <Avatar
                                     alt={podcast.name}
@@ -31,6 +45,10 @@ export default function PodcastInformation({ channelId }) {
                                     sx={{
                                         height: 60,
                                         width: 60,
+                                        display: {
+                                            xs: 'none',
+                                            sm: 'inline-block',
+                                        }
                                     }}
                                 />
                                 <Typography 
@@ -38,6 +56,13 @@ export default function PodcastInformation({ channelId }) {
                                     component='h1'
                                     fontFamily='Roboto, cursive'
                                     fontWeight='500'
+                                    sx={{
+                                        textAlign: {
+                                            xs: 'center',
+                                            sm: 'left'
+                                        },
+                                        flexGrow: 1
+                                    }}
                                 >
                                     {podcast.name}
                                 </Typography>
