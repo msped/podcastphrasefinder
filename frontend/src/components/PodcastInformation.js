1598af67850d79fc35793b45ba0d408b97e1fd11
Matt Edwards <mattsp.edwards@googlemail.com>
2023-10-21 23:12:17 +0100

    feat: added meta description and title to podcast information for podcast page

--- frontend/src/components/PodcastInformation.js
+++ frontend/src/components/PodcastInformation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 import {
     Typography,
     Card,
@@ -17,60 +18,73 @@ export default function PodcastInformation({ channelId }) {
     const youtubeChannelLink = `https://www.youtube.com/channel/${channelId}`
 
     return (
-        <Card>
-            {isLoading || !podcast ? (
-                <PodcastInformationSkeleton />
-            ) : (
-                <CardActionArea href={youtubeChannelLink} target='_blank'>
-                    <CardContent sx={{ padding: 3 }}>
-                        <Stack direction='column' spacing={1}>
-                            <Stack alignItems='center' justifyItems='center'>
-                                <Avatar
-                                    alt={podcast.name}
-                                    src={podcast.avatar}
-                                    sx={{
-                                        height: 60,
-                                        width: 60,
-                                        display: {
-                                            xs: 'inline-block',
-                                            sm: 'none'
-                                        }
-                                    }}
-                                />
+        <>
+            <Head>
+                <title>
+                    Find the episode of { 
+                    podcast.name ? podcast.name : 'your favourite podcast'
+                    } based on what was said. | PodFinder
+                </title>
+                <meta name="description" content="Find the episode you heard a phrase 
+                from by searching through podcast transcripts. Easily locate the episode
+                and show using PodFinder."
+                />
+            </Head>
+            <Card>
+                {isLoading || !podcast ? (
+                    <PodcastInformationSkeleton />
+                ) : (
+                    <CardActionArea href={youtubeChannelLink} target='_blank'>
+                        <CardContent sx={{ padding: 3 }}>
+                            <Stack direction='column' spacing={1}>
+                                <Stack alignItems='center' justifyItems='center'>
+                                    <Avatar
+                                        alt={podcast.name}
+                                        src={podcast.avatar}
+                                        sx={{
+                                            height: 60,
+                                            width: 60,
+                                            display: {
+                                                xs: 'inline-block',
+                                                sm: 'none'
+                                            }
+                                        }}
+                                    />
+                                </Stack>
+                                <Stack direction='row' spacing={2} marginY={1}>
+                                    <Avatar
+                                        alt={podcast.name}
+                                        src={podcast.avatar}
+                                        sx={{
+                                            height: 60,
+                                            width: 60,
+                                            display: {
+                                                xs: 'none',
+                                                sm: 'inline-block',
+                                            }
+                                        }}
+                                    />
+                                    <Typography 
+                                        variant='h3'
+                                        component='h1'
+                                        fontFamily='Roboto, cursive'
+                                        fontWeight='500'
+                                        sx={{
+                                            textAlign: {
+                                                xs: 'center',
+                                                sm: 'left'
+                                            },
+                                            flexGrow: 1
+                                        }}
+                                    >
+                                        {podcast.name}
+                                    </Typography>
+                                </Stack>
                             </Stack>
-                            <Stack direction='row' spacing={2} marginY={1}>
-                                <Avatar
-                                    alt={podcast.name}
-                                    src={podcast.avatar}
-                                    sx={{
-                                        height: 60,
-                                        width: 60,
-                                        display: {
-                                            xs: 'none',
-                                            sm: 'inline-block',
-                                        }
-                                    }}
-                                />
-                                <Typography 
-                                    variant='h3'
-                                    component='h1'
-                                    fontFamily='Roboto, cursive'
-                                    fontWeight='500'
-                                    sx={{
-                                        textAlign: {
-                                            xs: 'center',
-                                            sm: 'left'
-                                        },
-                                        flexGrow: 1
-                                    }}
-                                >
-                                    {podcast.name}
-                                </Typography>
-                            </Stack>
-                        </Stack>
-                    </CardContent>
-                </CardActionArea>
-            )}
-        </Card>
+                        </CardContent>
+                    </CardActionArea>
+                )}
+            </Card>
+        </>
     );
 }
