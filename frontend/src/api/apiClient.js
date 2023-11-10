88fbf2c3a3d6993d3496534aab0c915051456fd2
Matt Edwards <mattsp.edwards@googlemail.com>
2023-11-10 17:30:09 +0000

    refactor: change api route to env

--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: "http://127.0.0.1:8000/api/",
+    baseURL: `${process.env.API_HOST}/api/`,
     headers: {"Content-Type": "application/json",
         accept: "application/json",
     },
