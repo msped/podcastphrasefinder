efd17d3c4b9f57a79bda0d5855933cc30c98b3e5
Matt Edwards <mattsp.edwards@googlemail.com>
2023-11-11 00:34:40 +0000

    refactor: commented out debug toolbar url

--- backend/urls.py
+++ backend/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('podcasts.urls')),
-    path("__debug__/", include("debug_toolbar.urls")),
+    # path("__debug__/", include("debug_toolbar.urls")),
 ]
