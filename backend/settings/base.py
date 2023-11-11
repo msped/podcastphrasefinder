e4eab7fd3141e00ddd464359c4bcbbfdece6e68f
Matt Edwards <mattsp.edwards@googlemail.com>
2023-11-11 00:58:35 +0000

    build: removed static urls / root move to dev or prod settings

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -110,15 +110,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 APPEND_SLASH = False
