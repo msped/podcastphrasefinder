0e5c753a44b5ed74663a6417b9108ee194b01285
Matt Edwards <mattsp.edwards@googlemail.com>
2023-11-16 23:41:20 +0000

    fix: recursion error on signal

--- podcasts/signals.py
+++ podcasts/signals.py
@@ -6,12 +6,15 @@ from .models import Podcast
 from .tasks import add_back_catalogue_task
 from .utils import get_avatar
 
+
 @receiver(post_save, sender=Podcast)
-def add_back_catalogue_of_channel(sender, instance, **kwargs):
+def add_back_catalogue_of_channel(sender, instance, update_fields=None, **kwargs):
     if connection.settings_dict['NAME'] != 'testdatabase':
         if not instance.avatar:
             instance.avatar = get_avatar(instance.channel_id)
+            instance.save(update_fields=['avatar'])
         if instance.run_auto_add_back_catalogue and not instance.has_add_back_catalogue_ran:
-            add_back_catalogue_task.delay(instance.id, instance.channel_id, instance.video_filter)
+            add_back_catalogue_task.delay(
+                instance.id, instance.channel_id, instance.video_filter)
             instance.has_add_back_catalogue_ran = True
-        instance.save()
+            instance.save(update_fields=['has_add_back_catalogue_ran'])
