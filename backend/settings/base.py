e15e341014c835e90c1e02c01f059be4804cb18d
Matt Edwards <mattsp.edwards@googlemail.com>
2024-01-07 16:24:04 +0000

    feat: added allauth google provider to installed apps

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
     "dj_rest_auth",
     "dj_rest_auth.registration",
 
