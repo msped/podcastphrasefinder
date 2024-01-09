c6b9da30b66acd36dbde72be1a7efefe9f39737f
Matt Edwards <mattsp.edwards@googlemail.com>
2024-01-09 20:25:24 +0000

    refactor: remove temp sign out for menu

--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -1,24 +1,17 @@
 import React from 'react'
-import { signIn, signOut, useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import {
-    Stack,
-    Typography,
     Button
 } from "@mui/material";
+
+import AccountMenu from '@/components/AccountMenu';
 
 export default function SignIn() {
     const { data: session, status } = useSession();
 
     if (session) {
         return (
-            <Stack>
-                <Typography>
-                    Signed in as {session.user.email}
-                </Typography>
-                <Button onClick={() => signOut()}>
-                    Sign out
-                </Button>
-            </Stack>
+            <AccountMenu />
         )
     }
 
