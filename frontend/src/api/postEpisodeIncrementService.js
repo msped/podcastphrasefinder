3a9d89d7c4fdc158b6af2eb10876da75361cb512
Matt Edwards <mattsp.edwards@googlemail.com>
2024-01-26 22:22:58 +0000

    feat: remove postEpisodeIncrementService as not needed

--- frontend/src/api/postEpisodeIncrementService.js
+++
@@ -1,9 +0,0 @@
-import apiClient from "./apiClient";
-
-const postEpisodeIncrementService = (episodeId) => {
-    return apiClient
-        .post(`podcasts/episode/increment/${episodeId}`)
-        .then((res) => res.data)
-}
-
-export default postEpisodeIncrementService;
