434a9a84553645696b2f157efa1f17ed1cebc855
Matt Edwards <mattsp.edwards@googlemail.com>
2024-01-26 20:18:03 +0000

    feat: added creator url to project urls

--- backend/urls.py
+++ backend/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
     path('api/', include('podcasts.urls')),
+    path('api/creator/', include('creatoradmin.urls')),
     # path("__debug__/", include("debug_toolbar.urls")),
 ]
