64f9888e6b63292185104cdcb30ad72dfa0db6f6
Matt Edwards <mattsp.edwards@googlemail.com>
2024-02-04 22:48:50 +0000

    fix: changed from http to https in get_video_id

--- creatoradmin/utils.py
+++ creatoradmin/utils.py
@@ -3,7 +3,7 @@ from urllib.parse import urlparse, parse_qs
 
 def get_video_id(youtube_url):
     if not youtube_url.startswith(('http://', 'https://')):
-        youtube_url = f'http://{youtube_url}'
+        youtube_url = f'https://{youtube_url}'
     parsed_url = urlparse(youtube_url)
     video_id = parse_qs(parsed_url.query).get('v')
     return video_id[0] if video_id else None
