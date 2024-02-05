9ada9c8587e1a5ffe7b6a9eb3dc6590a9849b3b6
Matt Edwards <mattsp.edwards@googlemail.com>
2024-02-05 22:42:34 +0000

    tests: added tests for convert_date_from_picker function

--- creatoradmin/tests/test_utils.py
+++ creatoradmin/tests/test_utils.py
@@ -1,6 +1,6 @@
 from rest_framework.test import APITestCase
 
-from ..utils import get_video_id
+from ..utils import get_video_id, convert_date_from_picker
 
 
 class TestUtilsFunctions(APITestCase):
@@ -24,3 +24,15 @@ class TestUtilsFunctions(APITestCase):
     def test_empty_string(self):
         response = get_video_id("")
         self.assertIsNone(response)
+
+    def test_convert_valid_date(self):
+        self.assertEqual(convert_date_from_picker(
+            '12/25/2020'), '2020-12-25T00:00:01Z')
+
+    def test_convert_invalid_date_format(self):
+        with self.assertRaises(ValueError):
+            convert_date_from_picker('25/12/2020')
+
+    def test_convert_empty_string(self):
+        with self.assertRaises(ValueError):
+            convert_date_from_picker('')
