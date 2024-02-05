647ad2ed6785a82a228cb7ceda8a21adc4153215
Matt Edwards <mattsp.edwards@googlemail.com>
2024-02-05 22:33:57 +0000

    feat: added function convert date picker to formatted date in postgres

--- creatoradmin/utils.py
+++ creatoradmin/utils.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from urllib.parse import urlparse, parse_qs
 
 
@@ -7,3 +8,9 @@ def get_video_id(youtube_url):
     parsed_url = urlparse(youtube_url)
     video_id = parse_qs(parsed_url.query).get('v')
     return video_id[0] if video_id else None
+
+
+def convert_date_from_picker(unformatted_date):
+    new_date = datetime.strptime(
+        unformatted_date, '%m/%d/%Y').strftime('%Y-%m-%d')
+    return f'{str(new_date)}T00:00:01Z'
