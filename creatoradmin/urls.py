4f9a37ceabb51d2105a927fb570f403749fade8e
Matt Edwards <mattsp.edwards@googlemail.com>
2024-02-08 22:55:55 +0000

    feat: added AddYouTubeEpisode url to app urls

--- creatoradmin/urls.py
+++ creatoradmin/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import YouTubeVideoIdCheck
+from .views import YouTubeVideoIdCheck, AddYouTubeEpisode
 
 urlpatterns = [
+    path('youtube/add', AddYouTubeEpisode.as_view()),
     path('youtube/add/check', YouTubeVideoIdCheck.as_view(),
          name='youtube_add_check'),
 ]
