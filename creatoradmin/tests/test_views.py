e3aae0b9285ea6e9395936fe0d78dce7d68c14d0
Matt Edwards <mattsp.edwards@googlemail.com>
2024-02-09 22:01:17 +0000

    test: added tests for AddYouTubeEpisode view

--- creatoradmin/tests/test_views.py
+++ creatoradmin/tests/test_views.py
@@ -1,9 +1,11 @@
-from unittest.mock import MagicMock, patch
+from unittest.mock import patch
 from django.contrib.auth.models import User
 from rest_framework.test import APITestCase, APIClient
 
+from podcasts.models import Podcast, Episode
 
-class TestEpisodeRelatedViews(APITestCase):
+
+class TestVideoIdCheck(APITestCase):
     def setUp(self):
         self.client = APIClient()
         self.url = '/api/creator/youtube/add/check'
@@ -41,3 +43,61 @@ class TestEpisodeRelatedViews(APITestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data['text'], 'transcript')
         self.assertFalse(response.data['error'])
+
+
+class TestAddYoutubeEpisodeView(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.url = '/api/creator/youtube/add'
+        self.user = User.objects.create_user(
+            username='admin', password='admin')
+        self.client.force_authenticate(user=self.user)
+        self.podcast = Podcast.objects.create(
+            name='Test Podcast',
+            channel_id='testtesttest',
+            avatar='https://test.test/'
+        )
+
+    @patch('creatoradmin.utils.get_video_id')
+    @patch('creatoradmin.utils.convert_date_from_picker')
+    def test_add_youtube_episode_valid(self, mock_get_video_id, mock_convert_date_from_picker):
+        mock_get_video_id.return_value = 'test9876'
+        mock_convert_date_from_picker.return_value = '2021-01-01T00:00:01Z'
+
+        payload = {
+            'title': 'New Episode',
+            'transcript': 'This is a transcript.',
+            'exclusive': '',
+            'url': 'https://www.youtube.com/watch?v=test9876',
+            'error_occurred': '',
+            'published_date': '01/01/2021',
+            'is_draft': ''
+        }
+
+        response = self.client.post(self.url, payload)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Episode.objects.count(), 1)
+        episode = Episode.objects.first()
+        self.assertEqual(episode.title, payload['title'])
+
+    @patch('creatoradmin.utils.get_video_id')
+    @patch('creatoradmin.utils.convert_date_from_picker')
+    def test_add_youtube_episode_invalid(self, mock_get_video_id, mock_convert_date_from_picker):
+        mock_get_video_id.return_value = 'test4567'
+        mock_convert_date_from_picker.return_value = '2021-01-01T00:00:01Z'
+
+        payload = {
+            'title': 'New Episode',
+            'transcript': 'This is a transcript.',
+            'exclusive': '',
+            'url': '',  # no url will provide video_id error
+            'error_occurred': '',
+            'published_date': '01/01/2021',
+            'is_draft': ''
+        }
+
+        response = self.client.post(self.url, payload)
+
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('video_id', response.data)
