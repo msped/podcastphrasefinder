e0262522d0fbacebafb2a440a8e2b70c73db5e87
Matt Edwards <mattsp.edwards@googlemail.com>
2024-03-15 19:30:09 +0000

    test: added test for creator/episode/add page

---
+++ frontend/src/__tests__/pages/CreatorAdd.test.js
@@ -0,0 +1,37 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import "@testing-library/jest-dom";
+import Add from '@/pages/creator/episode/add'; 
+
+jest.mock('../../forms/AddEpisodeFromYouTubeForm', () => () => <div>AddEpisodeFromYouTubeForm Component</div>);
+
+describe('Add Component Tests', () => {
+    test('renders Add an Episode header', () => {
+        render(<Add />);
+        const headerElement = screen.getByRole('heading', {
+            name: /add an episode/i,
+        });
+        expect(headerElement).toBeInTheDocument();
+    });
+
+    test('renders TabPanel with correct tab selected', () => {
+        render(<Add />);
+        expect(screen.getByText("AddEpisodeFromYouTubeForm Component")).toBeVisible();
+
+        const youtubeTabButton = screen.getByRole('tab', {
+            name: /youtube/i,
+        });
+        expect(youtubeTabButton).toHaveAttribute('aria-selected', 'true');
+    });
+
+    test('handles tab change correctly', () => {
+        render(<Add />);
+        
+        const transcriptTabButton = screen.getByRole('tab', {
+            name: /get a transcript/i,
+        });
+
+        fireEvent.click(transcriptTabButton);
+        expect(transcriptTabButton).toHaveAttribute('aria-selected', 'false');
+        expect(screen.getByText("AddEpisodeFromYouTubeForm Component")).toBeVisible();
+    });
+});
