7a4f752d9a2fac05e11fb43206d18e38e58d24ee
Matt Edwards <mattsp.edwards@googlemail.com>
2024-03-23 15:59:40 +0000

    feat: added next google font

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,10 +1,17 @@
 import * as React from 'react';
 import Head from 'next/head';
+import { Roboto } from 'next/font/google';
 import CssBaseline from '@mui/material/CssBaseline';
 import Header from '@/components/Header'
 import Footer from '@/components/Footer';
 import { Toaster } from 'react-hot-toast';
 import Providers from '@/components/Providers';
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['400', '500', '700', '900']
+})
 
 export default function MyApp(props) {
 
@@ -16,22 +23,22 @@ export default function MyApp(props) {
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <CssBaseline />
-      <Toaster
-        position="bottom-left"
-        reverseOrder={true}
-        toastOptions={{
-          style: {
-            borderRadius: '10px',
-            background: '#333',
-            color: '#fff'
-          }
-        }}
-      />
-      <Header />
-      <main style={{ minHeight: '80vh' }}>
-        <Component {...pageProps} />
+      <main style={{ minHeight: '80vh' }} className={roboto.className}>
+        <Toaster
+          position="bottom-left"
+          reverseOrder={true}
+          toastOptions={{
+            style: {
+              borderRadius: '10px',
+              background: '#333',
+              color: '#fff'
+            }
+          }}
+        />
+        <Header />
+          <Component {...pageProps} />
+        <Footer />
       </main>
-      <Footer />
     </Providers>
   );
 }
