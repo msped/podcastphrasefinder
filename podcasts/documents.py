09acb63d6756eab9ab391c25b076695072b99beb
Matt Edwards <mattsp.edwards@googlemail.com>
2024-04-01 23:24:02 +0100

    feat: changed EpisodeDocument to Transcript document to search Transcript model

--- podcasts/documents.py
+++ podcasts/documents.py
@@ -1,36 +1,41 @@
 from django_elasticsearch_dsl import Document, fields
 from django_elasticsearch_dsl.registries import registry
 
-from .models import Episode
+from .models import Transcript
 
 
 @registry.register_document
-class EpisodeDocument(Document):
+class TranscriptDocument(Document):
 
-    channel = fields.ObjectField(
+    episode = fields.ObjectField(
         properties={
-            "name": fields.TextField(),
-            "slug": fields.TextField(),
-            "channel_id": fields.TextField(),
-            "avatar": fields.TextField()
+            "id": fields.IntegerField(),
+            "channel": fields.ObjectField(
+                properties={
+                    "name": fields.TextField(),
+                    "slug": fields.TextField(),
+                    "channel_id": fields.TextField(),
+                    "avatar": fields.TextField()
+                }
+            ),
+            "title": fields.TextField(),
+            "published_date": fields.TextField(),
+            "private_video": fields.BooleanField(),
+            "exclusive": fields.BooleanField(),
         }
     )
 
     class Index:
-        name = "episodes"
+        name = "transcripts"
         settings = {
             'number_of_shards': 1,
             'number_of_replicas': 0
         }
 
     class Django:
-        model = Episode
+        model = Transcript
         fields = [
             'id',
-            'title',
-            'transcript',
-            'published_date',
-            'private_video',
             'error_occurred',
-            'exclusive',
+            'transcript'
         ]
