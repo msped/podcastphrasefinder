f7ac021e50ef358d23940a5973cb0cb92be42c5a
Matt Edwards <mattsp.edwards@googlemail.com>
2024-04-01 17:08:22 +0100

    feat: added function to create transcript to utils

--- podcasts/utils.py
+++ podcasts/utils.py
@@ -5,7 +5,11 @@ import requests
 
 from youtube_transcript_api import YouTubeTranscriptApi
 
+from .models import Episode, Transcript
+from .serializers import EpisodeSerializer
+
 api_key = os.environ.get('YOUTUBE_V3_API_KEY')
+
 
 def call_api(url):
     try:
@@ -16,13 +20,16 @@ def call_api(url):
         print(f"An error occurred: {err}")
         return None
 
+
 def get_transcript(video_id):
     try:
         transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
-        transcript = " ".join([transcript['text'] for transcript in transcript_list])
+        transcript = " ".join([transcript['text']
+                              for transcript in transcript_list])
         return transcript, False
     except Exception as e:
         return str(e), True
+
 
 def check_for_private_video(video_id):
     sleep(5)
@@ -31,6 +38,7 @@ def check_for_private_video(video_id):
         timeout=10
     )
     return response.status_code == 404
+
 
 def get_avatar(channel_id):
     url_params = {
@@ -45,3 +53,24 @@ def get_avatar(channel_id):
     response = call_api(api_url)
     channel = response.get('items', [])
     return channel[0]['snippet']['thumbnails']['high']['url']
+
+
+def create_transcript_models(video_data):
+    if len(video_data) > 0:
+        episodes = Episode.objects.bulk_create(
+            [Episode(**data) for data in video_data])
+        serializer = EpisodeSerializer(episodes, many=True, context={
+                                       'obtain_video_id': True})
+        for episode in serializer.data:
+            transcript, error = get_transcript(video_id=episode.video_id)
+            if len(transcript) > 3000:
+                Transcript.objects.create(
+                    episode_id=episode.id,
+                    transcript=transcript,
+                    error_occurred=error
+                )
+            else:
+                episode.private_video = True
+                episode.save(update_fields=['private_video'])
+        return serializer.data
+    return 'No episodes found.'
