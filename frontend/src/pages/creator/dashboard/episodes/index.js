a2fee92e88561a25fb6fd9eb36c3518ea814046e
Matt Edwards <mattsp.edwards@googlemail.com>
2024-05-06 18:52:18 +0100

    feat: added page for routing

---
+++ frontend/src/pages/creator/dashboard/episodes/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import withDashboardLayout from '../../_components/withDashboardLayout'
+
+function EpisodesDashboard() {
+    return (
+        <div>index</div>
+    )
+}
+
+export default withDashboardLayout(EpisodesDashboard)
