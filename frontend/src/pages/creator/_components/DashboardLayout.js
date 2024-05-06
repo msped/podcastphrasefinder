3147e2306717223d101b43282728928d2dbabc39
Matt Edwards <mattsp.edwards@googlemail.com>
2024-05-06 18:38:52 +0100

    feat: added DashboardLayout

--- frontend/src/pages/creator/_components/DashboardLayout.js
+++ frontend/src/pages/creator/_components/DashboardLayout.js
@@ -11,14 +11,23 @@ import AddIcon from '@mui/icons-material/Add';
 import ViewListIcon from '@mui/icons-material/ViewList';
 import Link from 'next/link';
 import { usePathname } from "next/navigation";
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
 
 export default function DashboardLayout({ children }) {
+    const theme = useTheme();
     const pathname = usePathname();
+
+    const isDesktop = useMediaQuery(theme.breakpoints.up('md')) 
 
     return (
         <Container maxWidth='xl'>
-            <Box sx={{ display: 'flex', py: 5 }}>
-                <Stack direction='column' spacing={1}>
+            <Box sx={{ display: isDesktop ? 'flex' : 'block', py: 2 }}>
+                <Stack 
+                    direction={ isDesktop ? 'column' : 'row' } 
+                    spacing={1}
+                    sx={{ py: isDesktop ? 0 : 2 }}
+                >
                     <Link href='/creator/dashboard/episodes/add' passHref>
                         <Button
                             sx={{
