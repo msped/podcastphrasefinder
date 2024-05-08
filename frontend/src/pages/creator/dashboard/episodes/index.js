eefe0002aaab0ba497caf453b83df8fa99a183f5
Matt Edwards <mattsp.edwards@googlemail.com>
2024-05-08 21:34:35 +0100

    feat: setup table for episode dashboard

--- frontend/src/pages/creator/dashboard/episodes/index.js
+++ frontend/src/pages/creator/dashboard/episodes/index.js
@@ -1,9 +1,94 @@
 import React from 'react'
 import withDashboardLayout from '../../_components/withDashboardLayout'
+import {
+    Box,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TablePagination,
+    TableRow,
+} from '@mui/material';
+import useGetCreatorEpisodesHook from '../../_hooks/useGetCreatorEpisodesHook';
+import LoadingSpinner from '@/components/LoadingSpinner';
+
+const columns = [
+    { id: 'title', label: 'Title', minWidth: 175 },
+    { id: 'exclusive', label: 'Exclusive', minWidth: 100 },
+    { id: 'published_date', label: 'Published Date', minWidth: 170 },
+    { id: 'private_video', label: 'Private Video', minWidth: 170 },
+    { id: 'is_draft', label: 'Draft', minWidth: 170 },
+];
 
 function EpisodesDashboard() {
+    const { results, isLoading } = useGetCreatorEpisodesHook();
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+    };
+
+    if (isLoading) {
+        return <LoadingSpinner />
+    }
     return (
-        <div>index</div>
+        <Box>
+            <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+                <TableContainer sx={{ maxHeight: 440 }}>
+                    <Table stickyHeader aria-label="sticky table">
+                        <TableHead>
+                            <TableRow>
+                            {columns.map((column) => (
+                                <TableCell
+                                key={column.id}
+                                style={{ minWidth: column.minWidth }}
+                                >
+                                    {column.label}
+                                </TableCell>
+                            ))}
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {results
+                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                            .map((row) => {
+                                return (
+                                <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                                    {columns.map((column) => {
+                                    const value = row[column.id];
+                                    return (
+                                        <TableCell key={column.id} align={column.align}>
+                                        {column.format && typeof value === 'number'
+                                            ? column.format(value)
+                                            : value}
+                                        </TableCell>
+                                    );
+                                    })}
+                                </TableRow>
+                                );
+                            })}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <TablePagination
+                    rowsPerPageOptions={[10, 25, 100]}
+                    component="div"
+                    count={results.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+                </Paper>
+        </Box>
     )
 }
 
