9f596ef1394199b7ea30dd0d4fbe656181bcb659
Matt Edwards <mattsp.edwards@googlemail.com>
2024-05-11 19:25:18 +0100

    feat: added org app views

--- organisations/views.py
+++ organisations/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Membership
+from .serializers import MembershipSerializer
+from podcasts.models import Podcast
+from podcasts.serializers import PodcastSerializer
 
-# Create your views here.
+
+class PodcastListCreateView(generics.ListCreateAPIView):
+    queryset = Podcast.objects.filter()
+    serializer_class = PodcastSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class MembershipListCreateView(generics.ListCreateAPIView):
+    queryset = Membership.objects.all()
+    serializer_class = MembershipSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class MembershipDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Membership.objects.all()
+    serializer_class = MembershipSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    lookup_field = 'username'
+    lookup_url_kwarg = 'username'
+
+    def perform_update(self, serializer):
+        serializer.save(user=self.request.user)
