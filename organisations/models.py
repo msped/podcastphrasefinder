f59829e51791da4a02dea18a2bb54bce9d326e1f
Matt Edwards <mattsp.edwards@googlemail.com>
2024-05-12 12:35:29 +0100

    refactor: moved role choices to global declaration

--- organisations/models.py
+++ organisations/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from podcasts.models import Podcast
 
+ROLE_CHOICES = (
+    ('Admin', 'Admin'),
+    ('Member', 'Member')
+)
+
 
 class Membership(models.Model):
-    ROLE_CHOICES = (
-        ('Admin', 'Admin'),
-        ('Member', 'Member')
-    )
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     podcast = models.ForeignKey(Podcast, on_delete=models.CASCADE)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
