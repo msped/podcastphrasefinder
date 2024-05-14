238db579b68a2aadfbeb6976c426ae0b9a3611d2
Matt Edwards <mattsp.edwards@googlemail.com>
2024-05-14 21:54:46 +0100

    tests: fixed tests for items refactored

--- frontend/src/__tests__/components/Header.test.js
+++ frontend/src/__tests__/components/Header.test.js
@@ -4,12 +4,15 @@ import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event'
 import { useSession } from 'next-auth/react';
 import Header from '@/components/Header';
+import useGetPodcastOrgsHook from '@/hooks/useGetPodcastOrgsHook';
 
 jest.mock("next-auth/react")
+jest.mock("../../hooks/useGetPodcastOrgsHook", () => jest.fn())
 
 describe('Header', () => {
     test('renders logo link', () => {
         useSession.mockReturnValue([false, false])
+        useGetPodcastOrgsHook.mockReturnValueOnce({})
         render(<Header />);
         const logoLink = screen.getByText(/PodcastPhraseFinder/i);
         expect(logoLink).toBeInTheDocument();
@@ -17,12 +20,14 @@ describe('Header', () => {
 
     test('renders episodes link', () => {
         useSession.mockReturnValue({data: null, status: 'unauthenticated'})
+        useGetPodcastOrgsHook.mockReturnValueOnce({})
         render(<Header />);
         const episodesLink = screen.getByRole('link', { name: /Episodes/i });
         expect(episodesLink).toBeInTheDocument();
     });
 
     test('renders podcasts link', () => {
+        useGetPodcastOrgsHook.mockReturnValueOnce({})
         useSession.mockReturnValue({data: null, status: 'unauthenticated'})
         render(<Header />);
         const podcastsLink = screen.getByRole('link', { name: /Podcasts/i });
@@ -30,6 +35,7 @@ describe('Header', () => {
     });
 
     test('renders a sign in button, when no session', () => {
+        useGetPodcastOrgsHook.mockReturnValueOnce({})
         useSession.mockReturnValue({data: null, status: 'unauthenticated'})
         render(<Header />);
         const signInBtn = screen.getByRole('button', { name: /Sign in/i });
@@ -37,6 +43,7 @@ describe('Header', () => {
     });
 
     test('renders a sign out button, when there is a session', async () => {
+        useGetPodcastOrgsHook.mockReturnValueOnce({})
         useSession.mockReturnValue({
             data: {
                 expires: new Date(Date.now() + 2 * 86400).toISOString(),
@@ -54,5 +61,17 @@ describe('Header', () => {
 
         const signOutOption = screen.getByText(/sign out/i);
         expect(signOutOption).toBeInTheDocument();
+    });
+
+    it('renders the AccountMenu when there is a session', () => {
+        useSession.mockReturnValue({
+            data: { user: { name: 'User', email: 'user@example.com' }},
+            status: 'authenticated'
+        });
+        useGetPodcastOrgsHook.mockReturnValueOnce({})
+
+        render(<Header />);
+        const accountMenuBtn = screen.getByLabelText(/account settings/i);
+        expect(accountMenuBtn).toBeInTheDocument();
     });
 });
