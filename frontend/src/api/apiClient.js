13252916b0f9f02d4ea805e5a418cb2e49201675
Matt Edwards <mattsp.edwards@googlemail.com>
2024-05-14 20:51:11 +0100

    refactor: apiClient to handle interceptors

--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -1,10 +1,35 @@
 import axios from "axios";
+import { getSession } from "next-auth/react";
 
-const apiClient = axios.create({
-    baseURL: `${process.env.NEXT_PUBLIC_API_HOST}/api/`,
-    headers: {"Content-Type": "application/json",
-        accept: "application/json",
-    },
-});
+const apiClient = () => {
+    const defaultOptions = {
+        baseURL: `${process.env.NEXT_PUBLIC_API_HOST}/api/`,
+        headers: {
+            "Content-Type": "application/json",
+            accept: "application/json",
+        },
+    }
+    
+    const instance = axios.create(defaultOptions);
 
-export default apiClient;
+    instance.interceptors.request.use(async (request) => {
+        const session = await getSession();
+        if (session) {
+            request.headers.Authorization = `Bearer ${session.access_token}`;
+        }
+        return request;
+    })
+
+    instance.interceptors.response.use(
+        (response) => {
+            return response;
+        },
+        (error) => {
+            console.log(`error`, error);
+        }
+    )
+
+    return instance;
+}
+
+export default apiClient();
