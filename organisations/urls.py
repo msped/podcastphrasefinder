6949d814904ce32b92d3efe9f70f5e0302765c62
Matt Edwards <mattsp.edwards@googlemail.com>
2024-09-04 01:12:03 +0100

    feat: added membership urls

--- organisations/urls.py
+++ organisations/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
-from .views import PodcastListCreateView, MembershipListCreateView, MembershipDetailView
+from .views import (
+    PodcastListCreateView,
+    MembershipListCreateView,
+    MembershipDetailView,
+    UserOrgSelectionView,
+)
 
 urlpatterns = [
-    path('', PodcastListCreateView.as_view(),
-         name='podcast-list-create'),
-    path('memberships', MembershipListCreateView.as_view(),
-         name='membership-list-create'),
-    path('memberships/<str:username>', MembershipDetailView.as_view(),
-         name='membership-detail'),
+    path('podcasts', PodcastListCreateView.as_view(), name='podcast-list-create'),
+    path('memberships/user', UserOrgSelectionView.as_view(),
+         name='user-org-selection'),
+    path('memberships', MembershipListCreateView.as_view(), name='membership-list'),
+    path('memberships/<int:id>',
+         MembershipDetailView.as_view(), name='membership-detail'),
 ]
