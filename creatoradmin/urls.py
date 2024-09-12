29177ea85ada6b8f0fdb7ab5236252617a6b32a5
Matt Edwards <mattsp.edwards@googlemail.com>
2024-09-12 20:08:42 +0100

    feat: added episode detail url

--- creatoradmin/urls.py
+++ creatoradmin/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 
-from .views import YouTubeVideoIdCheck, AddYouTubeEpisode, CreatorEpisodes
+from .views import (
+    YouTubeVideoIdCheck,
+    AddYouTubeEpisode,
+    CreatorEpisodes,
+    BulkDeleteEpisodes,
+)
 
 urlpatterns = [
     path('youtube/add', AddYouTubeEpisode.as_view()),
     path('youtube/add/check', YouTubeVideoIdCheck.as_view(),
          name='youtube_add_check'),
     path('<slug:slug>/episodes', CreatorEpisodes.as_view(), name='creator_episodes'),
+    path('episodes/<pk>',
+         BulkDeleteEpisodes.as_view(), name='bulk_deleteepisodes'),
 ]
