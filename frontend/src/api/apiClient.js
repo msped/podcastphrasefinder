f6f2d8dff21efa030e1b1323bbf0c57d4bc104ad
Matt Edwards <mattsp.edwards@googlemail.com>
2024-09-17 23:42:36 +0100

    refactor: added return for error promise rejections

--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -25,7 +25,7 @@ const apiClient = () => {
             return response;
         },
         (error) => {
-            console.log(`error`, error);
+            return Promise.reject(error)
         }
     )
 
