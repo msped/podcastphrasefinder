cf05ff1ce5b2c2a6adcedfc791a37a50074d69ce
Matt Edwards <mattsp.edwards@googlemail.com>
2024-09-24 23:44:02 +0100

    feat: added useGetEpisodeHook

---
+++ frontend/src/pages/creator/_hooks/useGetEpisodeHook.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import getEpisodeService from '@/pages/creator/_api/getEpisodeService';
+
+const useGetEpisodeHook = () => {
+    const [episodeId, setEpisodeId] = useState(null);
+    const [episode, setEpisode] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchDataFromService = async () => {
+            setIsLoading(true);
+            try {
+                const data = await getEpisodeService(episodeId);
+                setEpisode(data);
+                setError(null);
+            } catch (err) {
+                setError(err.message);
+                setEpisode(null);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        if (episodeId !== null) {
+            fetchDataFromService();
+        } else {
+            setIsLoading(false);
+            setEpisode(null);
+        }
+    }, [episodeId]);
+
+    return { episode, isLoading, error, setEpisodeId, setEpisode };
+};
+
+export default useGetEpisodeHook;
