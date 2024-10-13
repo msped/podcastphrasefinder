0dae2c490f49518b51663400f4d612e7877d0dd0
Matt Edwards <mattsp.edwards@googlemail.com>
2024-10-13 22:16:09 +0100

    feat: added podcast instance to get_podcast_or_channel

--- organisations/permissions.py
+++ organisations/permissions.py
@@ -1,7 +1,10 @@
 from rest_framework import permissions
+from podcasts.models import Podcast
 
 
 def _get_podcast_or_channel(obj):
+    if isinstance(obj, Podcast):
+        return obj
     return getattr(
         obj, 'podcast',
         getattr(obj, 'channel',
@@ -24,7 +27,6 @@ class IsOrgAdmin(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-
         podcast_or_channel = _get_podcast_or_channel(obj)
         if podcast_or_channel is None:
             return False
