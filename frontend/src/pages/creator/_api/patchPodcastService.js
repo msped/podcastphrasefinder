5ae0bb03d67d5fa66c0487e3f8df656089372c42
Matt Edwards <mattsp.edwards@googlemail.com>
2024-10-20 16:39:17 +0100

    fix: added content-type to patchPodcastService for avatar only form

--- frontend/src/pages/creator/_api/patchPodcastService.js
+++ frontend/src/pages/creator/_api/patchPodcastService.js
@@ -2,7 +2,11 @@ import apiClient from "../../../api/apiClient";
 
 const patchPodcastService = async (podcastSlug, formData) => {
     const res = await apiClient
-        .patch(`orgs/podcasts/${podcastSlug}`, formData);
+        .patch(`orgs/podcasts/${podcastSlug}`, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        });
     return res;
 }
 
