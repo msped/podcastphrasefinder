d70a58850b104e9a087f3b046e7a06ca9acf3d99
Matt Edwards <mattsp.edwards@googlemail.com>
2025-01-04 23:29:49 +0000

    feat: added TransferOwnershipService to membershipServices

--- frontend/src/pages/creator/_api/membershipServices.js
+++ frontend/src/pages/creator/_api/membershipServices.js
@@ -23,3 +23,9 @@ export const deleteMembershipsService = async (memberId) => {
         .delete(`/orgs/memberships/${memberId}`);
     return res;
 }
+
+export const TransferMembershipService = async (formData) => {
+    const res = await apiClient
+        .post('orgs/podcasts/transfer/ownership', {'requested_owner': formData});
+    return res;
+}
