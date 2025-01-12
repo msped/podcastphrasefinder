5ef3e336a2edc1e6fa28929428be160486776899
Matt Edwards <mattsp.edwards@googlemail.com>
2025-01-12 23:33:22 +0000

    build: added email config to django

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -206,3 +206,12 @@ SOCIALACCOUNT_PROVIDERS = {
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = 'noreply@podcastphrasefinder.com'
+EMAIL_SUBJECT_PREFIX = ''
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_ADDRESS')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_PORT = 587
