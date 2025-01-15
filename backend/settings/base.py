2371ba3e1ec55cb9783c270a128d9412b0c91564
Matt Edwards <mattsp.edwards@googlemail.com>
2025-01-15 15:57:56 +0000

    fix: added SSL_CERT_FILE declaration to settings.py

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 import os
+import certifi
 from pathlib import Path
 from dotenv import load_dotenv
 from celery.schedules import crontab
@@ -208,10 +209,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-DEFAULT_FROM_EMAIL = 'noreply@podcastphrasefinder.com'
+DEFAULT_FROM_EMAIL = 'matt@mspe.me'
 EMAIL_SUBJECT_PREFIX = ''
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_ADDRESS')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT = 587
+
+os.environ['SSL_CERT_FILE'] = certifi.where()
