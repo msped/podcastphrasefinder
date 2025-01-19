3c6caa1ffec3ee7b807d09e01e327a3e261104c6
Matt Edwards <mattsp.edwards@googlemail.com>
2025-01-19 18:11:07 +0000

    feat: added DeletePodcast mock to CreatorPodcastSettings test

--- frontend/src/__tests__/pages/CreatorPodcastSettings.test.js
+++ frontend/src/__tests__/pages/CreatorPodcastSettings.test.js
@@ -7,6 +7,7 @@ import Settings from '@/pages/creator/podcast/settings';
 jest.mock('../../pages/creator/_components/withDashboardLayout', () => (Component) => Component);
 jest.mock('../../pages/creator/_components/PodcastSettings');
 jest.mock('../../pages/creator/_components/UserManagement')
+jest.mock('../../pages/creator/_components/DeletePodcast')
 jest.mock('../../api/getOrgSelectionService', );
 jest.mock('../../components/LoadingSpinner');
 
