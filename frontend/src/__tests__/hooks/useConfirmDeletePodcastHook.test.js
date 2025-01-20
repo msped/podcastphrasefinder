f915884bc1a547bc01f245f1d7546dc54e829901
Matt Edwards <mattsp.edwards@googlemail.com>
2025-01-20 20:11:29 +0000

    feat: added useConfirmDeletePodcastHook and associated tests

---
+++ frontend/src/__tests__/hooks/useConfirmDeletePodcastHook.test.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { render, act, fireEvent, waitFor } from '@testing-library/react';
+import useConfirmDeletePodcastHook from '@/pages/creator/_hooks/useConfirmDeletePodcastHook';
+import getConfirmDeletePodcastService from '@/pages/creator/_api/getConfirmDeletePodcastService';
+
+// Mock the service
+jest.mock('../../pages/creator/_api/getConfirmDeletePodcastService');
+
+const TestComponent = () => {
+    const { status, error, isLoading, setSlug, setToken } = useConfirmDeletePodcastHook();
+
+    return (
+        <div>
+            <span data-testid="status">{status}</span>
+            <span data-testid="error">{error?.error}</span>
+            <span data-testid="isLoading">{isLoading ? 'loading' : 'loaded'}</span>
+            <button onClick={() => setSlug('test-slug')} data-testid="setSlugButton">Set Slug</button>
+            <button onClick={() => setToken('test-token')} data-testid="setTokenButton">Set Token</button>
+        </div>
+    );
+};
+
+describe('useConfirmDeletePodcastHook', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should have initial states', () => {
+        const { getByTestId } = render(<TestComponent />);
+
+        expect(getByTestId('status').textContent).toBe('');
+        expect(getByTestId('error').textContent).toBe('');
+        expect(getByTestId('isLoading').textContent).toBe('loading');
+    });
+
+    it('should fetch and update status on success', async () => {
+        const mockStatus = 200;
+        getConfirmDeletePodcastService.mockResolvedValue({ status: mockStatus });
+
+        const { getByTestId } = render(<TestComponent />);
+
+        await act(async () => {
+            fireEvent.click(getByTestId('setSlugButton'));
+            fireEvent.click(getByTestId('setTokenButton'));
+        });
+
+        await waitFor(() => expect(getByTestId('isLoading').textContent).toBe('loaded'));
+
+
+        expect(getConfirmDeletePodcastService).toHaveBeenCalledWith('test-slug', 'test-token');
+        expect(getByTestId('status').textContent).toBe(mockStatus.toString());
+        expect(getByTestId('error').textContent).toBe('');
+        expect(getByTestId('isLoading').textContent).toBe('loaded');
+    });
+
+    it('should handle an error correctly', async () => {
+        getConfirmDeletePodcastService.mockRejectedValue({ response: { status: 404, data: { 'error': 'Podcast does not exist.'}}});
+
+        const { getByTestId } = render(<TestComponent />);
+
+        await act(async () => {
+            fireEvent.click(getByTestId('setSlugButton'));
+            fireEvent.click(getByTestId('setTokenButton'));
+        });
+
+        await waitFor(() => expect(getByTestId('isLoading').textContent).toBe('loaded'));
+
+        expect(getConfirmDeletePodcastService).toHaveBeenCalledWith('test-slug', 'test-token');
+        expect(getByTestId('status').textContent).toBe('404');
+        expect(getByTestId('error').textContent).toBe('Podcast does not exist.');
+        expect(getByTestId('isLoading').textContent).toBe('loaded');
+    });
+});
---
+++ frontend/src/pages/creator/_hooks/useConfirmDeletePodcastHook.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import getConfirmDeletePodcastService from '@/pages/creator/_api/getConfirmDeletePodcastService';
+
+const useConfirmDeletePodcastHook = () => {
+    const [slug, setSlug] = useState(null);
+    const [token, setToken] = useState(null);
+    const [status, setStatus] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchDataFromService = async () => {
+            setIsLoading(true);
+            await getConfirmDeletePodcastService(slug, token)
+            .then(res => {
+                setStatus(res.status);
+                setError(null);
+            })
+            .catch(err => {
+                setError(err.response.data);
+                setStatus(err.response.status);
+            })
+            .finally(setIsLoading(false))
+            
+        };
+
+        if (slug && token) {
+            fetchDataFromService();
+        }
+    }, [slug, token]);
+
+    return { status, error, isLoading, setSlug, setToken };
+};
+
+export default useConfirmDeletePodcastHook;
