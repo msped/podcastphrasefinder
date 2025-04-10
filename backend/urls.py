5e8f09e2f46a33b6916f3c58eea38de764705f48
Matt Edwards <mattsp.edwards@googlemail.com>
2025-04-10 23:28:38 +0100

    build: added media url when debug is true, moved debug toolbar to same location

--- backend/urls.py
+++ backend/urls.py
@@ -10,5 +10,9 @@ urlpatterns = [
     path('api/', include('podcasts.urls')),
     path('api/creator/', include('creatoradmin.urls')),
     path('api/orgs/', include('organisations.urls')),
-    path("__debug__/", include("debug_toolbar.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += path("__debug__/", include("debug_toolbar.urls")),
